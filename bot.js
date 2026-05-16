de73ba0deabd3669cf724b7061514a2b5630fe2d
PachaiDizz <68173720+PachaiDizz@users.noreply.github.com>
2026-05-16 08:26:30 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,12 +20,12 @@ const ACCOUNTS = [
     sid: 'AC52a45e18747ad646fcbf4d68ab692f92',
     token: process.env.TWILIO_TOKEN_3 || '',
     from: 'whatsapp:+14155238886'
-  }
+  },
   {
       sid: 'ACdbf642024a8a0304a808a63cd9f16998',
       token: process.env.TWILIO_TOKEN_4 || '',
       from: 'whatsapp:+14155238886'
-  }  
+  },  
   
 ];
 
